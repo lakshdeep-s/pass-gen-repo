295672feead7316d8b6042c83e2d70c0afa43121
Lakshdeep Singh <lakshdeep.code@gmail.com>
2023-02-13 19:09:58 +0530

    Added Code Documentation and Minor Code changes

--- pass_generator.py
+++ pass_generator.py
@@ -1,40 +1,46 @@
 import random
-import os
 
 def passSpcCharacters():
     char_list = ['!','*','#','_','^','%','&','@','$']
     spc_string = str()
-    iter_var = 0
-    while iter_var < 4:
+    iter_var = 4
+    
+    #! Selecting (4) Random Special Characters
+    while iter_var:
         spc_string += str(random.choice(char_list))
-        iter_var+=1
+        iter_var-=1
     return spc_string
        
 def  passBaseString():
+    #! Opening Text File "random_text.txt" and acessing letters for password
     with open('random_text.txt','r') as file:
         allText = file.read()
         words = list(map(str, allText.split()))
         
     base_string = random.choice(words)
-    if len(base_string) > 4:
-        ''.join(random.sample(base_string, len(base_string)))
-        base_string = base_string[0:4]
-        ''.join(random.choice((str.upper, str.lower))(char) for char in base_string)
-        return base_string
-    elif len(base_string) < 4:
-        passBaseString()
+    
+    #! Randomizing / Shuffling the base word
+    ''.join(random.sample(base_string, len(base_string)))
+    base_string = base_string[0:4]
+    
+    #! Randomizing the Case of the characters
+    ''.join(random.choice((str.upper, str.lower))(char) for char in base_string)
+    return base_string
     
 def passNumElements():
+    #! Selecting random digits 
     idealNum = random.randint(12345,98706)
-    return idealNum
+    return str(idealNum)
         
 def Generator():
     baseWord = passBaseString()
-    baseNum = str(passNumElements())
+    baseNum = passNumElements()
     baseSpcChars = passSpcCharacters()
     
+    #! Creating initial Password String
     initPassword = baseWord + baseNum + baseSpcChars
     
+    #! Randomizing again to return final password
     finalPassword = ''.join(random.sample(initPassword, len(initPassword)))
     ''.join(random.sample(finalPassword, len(finalPassword)))
     
